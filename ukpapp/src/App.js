d06fd871274083b900a781b210779bb3783d96b4
HimanshuJain2 <mail2jainhimanshu@gmail.com>
2023-12-21 17:19:10 +0530

    innnnn

---
+++ ukpapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ListingPage from './listing/ListingPage';
+
+function App() {
+  return (
+    <div>
+      <ListingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ukpapp/src/listing/ListingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+function ListingPage() {
+    return (
+        <div className='container listingPg'>
+            <div className='title'>
+                Property Type
+                <span className='mandatoryMark'>*</span>
+            </div>
+        </div>
+    )
+}
+
+export default ListingPage
